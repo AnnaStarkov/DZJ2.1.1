13263c8341098518c75dd6095a1fe63cc5ebc4ab
Anna Starkova <annastarkova2018@gmail.com>
2021-01-31 18:18:36 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 2000;
+        int rubles = 20;
+
+        int mili = ticket / rubles;
+        System.out.println(mili);
+    }
 }
